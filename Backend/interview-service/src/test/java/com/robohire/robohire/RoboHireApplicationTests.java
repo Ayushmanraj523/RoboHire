4fd30bbe15728c56fcb9b8797b4bdb754931ff16
Ayushmanraj523 <ayushmanraj925@gmail.com>
2026-02-19 01:32:39 +0530

    RoboHire

---
+++ Backend/interview-service/src/test/java/com/robohire/robohire/RoboHireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robohire.robohire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoboHireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
